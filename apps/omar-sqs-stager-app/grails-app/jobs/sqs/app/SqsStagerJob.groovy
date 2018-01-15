5d4b935496332c33a9c3e39931b1c750e0826a16
Garrett Potts <gpotts@radiantblue.com>
2018-01-15 11:47:03 -0500

    Added additional logging support for each stage of the staging process

--- apps/omar-sqs-stager-app/grails-app/jobs/sqs/app/SqsStagerJob.groovy
+++ apps/omar-sqs-stager-app/grails-app/jobs/sqs/app/SqsStagerJob.groovy
@@ -5,6 +5,7 @@ import groovy.json.JsonSlurper
 import omar.core.DateUtil
 import omar.avro.HttpUtils
 import omar.core.HttpStatus
+import groovy.json.JsonBuilder
 
 class SqsStagerJob {
    def sqsService
@@ -16,6 +17,15 @@ class SqsStagerJob {
    static triggers = {
       simple repeatInterval: 1000l, name: 'SqsReaderTrigger', group: 'SqsReaderGroup'
    }
+
+
+/*            def raster_logs = new JsonBuilder(timestamp: startTime.format("yyyy-MM-dd hh:mm:ss.ms"), requestType: requestType,
+                requestMethod: requestMethod, status: result?.status, message: result?.message,
+                filetypes: fileTypes, filenames: filenames, acquisitionDates: acquisitionDates,
+                ingestDates: ingestDates, missionids: missionids, imageids: imageids, sensorids: sensorids)
+
+            log.info raster_logs.toString()
+*/
   def execute() {
     def messages
     def config = SqsUtils.sqsConfig
@@ -38,39 +48,82 @@ class SqsStagerJob {
         //ingestdate = DateUtil.formatUTC(new Date())
         HashMap stagerParams = config.stager.params as HashMap
         messages?.each{message->
+          HashMap messageInfo = [ requestMethod: "SqsStagerJob",
+                                  messageId:null,
+                                  sourceUri: "",
+                                  filename: "",
+                                  downloadStartTime:null,
+                                  downloadEndTime: null,
+                                  downloadDuration: 0,
+                                  stageStartTime:null,
+                                  stageEndTime: null,
+                                  dataInfoStartTime:null,
+                                  dataInfoEndTime:null,
+                                  dataInfoDuration:0,
+                                  indexStartTime: null,
+                                  indexEndTime: null,
+                                  indexDuration: 0,
+                                  duration:0]
           try{
+            messageInfo.messageId = message?.messageId
+
             sqsService.deleteMessages(SqsUtils.sqsConfig.reader.queue,
                                       [message])
             if(sqsService.checkMd5(message.mD5OfBody, message.body))
             {
-              println "Got message and now parsing"
+              log.info "MessageId: ${messageInfo.messageId}"
               // log message start
               def jsonMessage = sqsService.parseMessage(message.body.toString())
-              println "Downloading"
+              log.info "MessageId: ${messageInfo.messageId}: Downloading....."
+
               // log message parsed
               def downloadResult = sqsService.downloadFile(jsonMessage)
-              if(downloadResult.message) println downloadResult.message
-              stagerParams.filename = downloadResult.destination
-              println "Staging"
+              messageInfo.downloadStartTime = downloadResult.startTime
+              messageInfo.downloadEndTime   = downloadResult.endTime
+              messageInfo.downloadDuration  = downloadResult.duration/1000
+              messageInfo.duration         += messageInfo.downloadDuration
+              messageInfo.sourceUri         = downloadResult.source 
+              messageInfo.filename          = downloadResult.destination 
+              stagerParams.filename         = downloadResult.destination
+              log.info "MessageId: ${messageInfo.messageId}: Download ${downloadResult.source} to ${downloadResult.destination}: ${downloadResult.message}"
+              log.info "MessageId: ${messageInfo.messageId}: Staging file ${stagerParams.filename} with message id: ${messageInfo.messageId}"
               def stageFileResult = sqsService.stageFileJni(stagerParams)
-              if(stageFileResult.message) println stageFileResult.message
-              println "Getting XML"
+              if(stageFileResult.status != HttpStatus.OK) log.error stageFileResult.message
+              messageInfo.stageStartTime = stageFileResult.startTime
+              messageInfo.stageEndTime = stageFileResult.endTime
+              messageInfo.stageDuration = stageFileResult.duration/1000
+              messageInfo.duration += messageInfo.stageDuration
+
+              log.info "MessageId: ${messageInfo.messageId}: Getting XML from file ${stagerParams.filename}"
               HashMap dataInfoResult = sqsService.getDataInfo(downloadResult.destination)
-              if(dataInfoResult.message) println dataInfoResult.message
+              messageInfo.dataInfoStartTime = dataInfoResult.startTime
+              messageInfo.dataInfoEndTime   = dataInfoResult.endTime
+              messageInfo.dataInfoDuration  = dataInfoResult.duration/1000
+              messageInfo.duration         += messageInfo.dataInfoDuration
+              if(dataInfoResult.status != HttpStatus.OK) log.error dataInfoResult.message
+
+
+              log.info "MessageId: ${messageInfo.messageId}: Indexing file ${stagerParams.filename}"
               HashMap addRasterResult = rasterDataSetService.addRasterXml(dataInfoResult?.xml)
-              ingestMetricsService.endIngest(stagerParams.filename)
-             
-              println addRasterResult
+              ingestMetricsService.endIngest(messageInfo.filename)
+              messageInfo.indexStartTime  = addRasterResult.startTime
+              messageInfo.indexEndTime    = addRasterResult.endTime
+              messageInfo.indexDuration   = addRasterResult.duration/1000
+              messageInfo.duration       += messageInfo.indexDuration
+              messageInfo.metadata        = addRasterResult.metadata
+              if(addRasterResult.status != HttpStatus.OK) log.error addRasterResult.message
+
+              log.info "MessageId: ${messageInfo.messageId}: Finished processing message ${messageInfo}"
+              log.info new JsonBuilder(messageInfo).toString()
             }
             else
             {
-              log.error "ERROR: BAD MD5 Checksum For Message: ${messageBody}"
+              log.error "MessageId: ${messageInfo.messageId} ERROR: BAD MD5 Checksum For Message: ${messageBody}"
             }
           }
           catch(e)
           {
-            //e.printStackTrace()
-            log.error "ERROR: ${e.toString()}"
+            log.error "MessageId: ${messageInfo.messageId} ERROR: ${e.toString()}"
           }
         }
       }
