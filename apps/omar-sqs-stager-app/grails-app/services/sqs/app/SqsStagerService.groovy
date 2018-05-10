6900a5e65120c0aa3d2da4d358aa780206a1d19a
arocnies <arocnies@gmail.com>
2018-05-10 14:50:02 -0400

    Switched `acquisitionToStartTime` with `secondsBeforeQueue`

--- apps/omar-sqs-stager-app/grails-app/services/sqs/app/SqsStagerService.groovy
+++ apps/omar-sqs-stager-app/grails-app/services/sqs/app/SqsStagerService.groovy
@@ -244,15 +244,8 @@ class SqsStagerService
 
         result.duration = (result.endTime.time - result.startTime.time)
 
-        // Calculate acquisitionToStartTime
+        // Include acquisition date in result so SqsStagerJob can use it.
         Date acquisitionDate = DateUtil.parseDate(jsonObj?."${OmarAvroUtils.avroConfig.dateField}")
-        TimeDuration acquisitionToStartTime = null
-        if (acquisitionDate instanceof Date) {
-            use(TimeCategory) {
-                acquisitionToStartTime = new Date() - acquisitionDate
-            }
-        }
-        result.acquisitionToStartTime = (acquisitionToStartTime.toMilliseconds() / 1000) // Convert to seconds.
         result.acquisitionDate = acquisitionDate
 
         return result
