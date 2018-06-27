d818857d1530bf77e8c30f1359e180b28adf3c23
Garrett Potts <gpotts@radiantblue.com>
2018-06-27 14:57:12 -0400

    Added an output to specify which files we are removing and cleaning off the system when we have a cancellation

--- apps/omar-sqs-stager-app/src/main/groovy/sqs/app/SqsStagerJobUtil.groovy
+++ apps/omar-sqs-stager-app/src/main/groovy/sqs/app/SqsStagerJobUtil.groovy
@@ -59,12 +59,10 @@ class SqsStagerJobUtil
             String fileOnly = file.name.lastIndexOf('.').with {it != -1 ? file.name[0..<it] : file.name}
             if(parent && fileOnly)
             {
-               new FileNameByRegexFinder().getFileNames(parent,"${file}*").each{
-                  File testFile = new File(it)
-                  if(testFile.exists())
-                  {
-                     testFile.delete()
-                  }
+               def filenames = new FileNameByRegexFinder().getFileNames(parent,"${file}*")
+               log.info "Cleaning filenames: ${filenames}"
+               filenames.each{tempFile->
+                  new File(tempFile).delete()
                }
             }
          }
