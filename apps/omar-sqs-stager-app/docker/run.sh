89b351cf99530d272dc76947c5ec8781720ff97f
Garrett Potts <gpotts@radiantblue.com>
2018-10-05 13:40:03 -0400

    Added nss wrapper

--- apps/omar-sqs-stager-app/docker/run.sh
+++ apps/omar-sqs-stager-app/docker/run.sh
@@ -1,40 +1,33 @@
 #!/bin/bash
-if [ ! -z $AWS_ACCESS_KEY ] ; then
-  export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY
-  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
-fi
-
-#export USER_ID=$(id -u)
-#export GROUP_ID=$(id -g)
-#sed '/^omar/d' /etc/passwd > /tmp/passwd
-#echo omar:x:$USER_ID:$GROUP_ID:Default Application User:$HOME:/sbin/nologin >> /tmp/passwd
-
-#export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
-#export NSS_WRAPPER_PASSWD=/tmp/passwd
-#export NSS_WRAPPER_GROUP=/etc/group
-if [ -z $MOUNT_POINT ] ; then
-  export MOUNT_POINT=/s3
-fi
-
-# force to forground
-#  we are taking a comma separated list of buckets in the form of
-#  AWS  <bucket>:<prefix-path>,.....
-#  where :<prefix-path> is optional.  
-#  we will mount to the location <mount-point>/<prefix-path>
+#
+# ENV Variables:
+#
+# GOOFY_OPTS - options for goofy
+# MOUNT_POINT - mount point for s3 buckets.  Should have default of /s3
 # 
-GOOFY_OPTS="-f ${GOOFY_OPTS}"
-if [ ! -z "${BUCKETS}" ] ; then
-  SPLIT_BUCKET=${BUCKETS//\,/ }
-  
-  for BUCKET in ${SPLIT_BUCKET} ; do
-    BUCKET_PATH="${MOUNT_POINT}/${BUCKET//://}"
-    mkdir -p $BUCKET_PATH
-    goofys ${GOOFY_OPTS} ${BUCKET} ${BUCKET_PATH} &
-  done
+# AWS_ACCESS_KEY - AWS key.  Optional
+# AWS_SECRET_KEY - AWS Secret Key. Optional
+# JAVA_OPTS - Arguments to pass to java to start the app.  Optional
+# 
+if [ ! -z "${AWS_ACCESS_KEY}" ] ; then
+  export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY}
 fi
 
-if [ "${JAVA_ARGS}" == "" ] ; then
-   export JAVA_ARGS="-server -Xms256m -Xmx1024m -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -XX:+UnlockExperimentalVMOptions  -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Djava.security.egd=file:/dev/./urandom"
+if [ ! -z "${AWS_SECRET_KEY}" ] ; then
+  export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_KEY}
+fi
+
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+sed '/^omar/d' /etc/passwd > /tmp/passwd
+echo omar:x:$USER_ID:$GROUP_ID:Default Application User:$HOME:/sbin/nologin >> /tmp/passwd
+
+export LD_PRELOAD=/usr/lib64/libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
+if [ -z "${JAVA_OPTS}" ] ; then
+   JAVA_OPTS="-server -Xms256m -Xmx1024m -Djava.awt.headless=true -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.security.egd=file:/dev/./urandom"
 fi
 
 if [ "${KEY_STORE}" != "" ] ; then
@@ -68,6 +61,33 @@ if [ "${TRUST_STORE_PASSWORD}" != "" ] ; then
       export JAVA_ARGS="-Djavax.net.ssl.trustStorePassword${TRUST_STORE_PASSWORD} ${JAVA_ARGS}"
    fi
 fi 
-export JAR_FILE=`find ${HOME} -name "*.jar"`
-echo "Running command: java ${JAVA_ARGS} -jar ${JAR_FILE}"
-java ${JAVA_ARGS} -jar ${JAR_FILE}
+
+if [ -z "${MOUNT_POINT}" ] ; then
+  export MOUNT_POINT=/s3
+fi
+
+if [ -z "${GOOFY_OPTS}" ] ; then
+   GOOFY_OPTS="-o allow_other"
+fi
+
+# force to forground
+#  we are taking a comma separated list of buckets in the form of
+#  AWS  <bucket>:<prefix-path>,.....
+#  where :<prefix-path> is optional.  
+#  we will mount to the location <mount-point>/<prefix-path>
+# 
+GOOFY_OPTS="-f ${GOOFY_OPTS}"
+if [ ! -z "${BUCKETS}" ] ; then
+  SPLIT_BUCKET=${BUCKETS//\,/ }
+  
+  for BUCKET in ${SPLIT_BUCKET} ; do
+    BUCKET_PATH="${MOUNT_POINT}/${BUCKET//://}"
+    mkdir -p $BUCKET_PATH
+    goofys ${GOOFY_OPTS} ${BUCKET} ${BUCKET_PATH} &
+  done
+fi
+
+export JAR_FILE=`find /home/omar -name "*.jar"`
+
+echo "java ${JAVA_OPTS} -jar ${JAR_FILE}"
+java ${JAVA_OPTS} -jar ${JAR_FILE}
